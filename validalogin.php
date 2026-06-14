1bf70037e42997467adfd50d713ee8fd8252b29c
Lázaro Tenório de Oliveira <163544630+lazaroliveira@users.noreply.github.com>
2026-06-13 23:08:07 -0300

    Add login functionality in validalogin.php

---
+++ validalogin.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'conexao.php';
+require_once 'classes.php';
+
+$auth = new Auth($pdo);
+
+$usuario_input = $_POST['usuario'] ?? '';
+$senha_input   = $_POST['senha']   ?? '';
+
+if ($auth->login($usuario_input, $senha_input)) {
+    header('Location: livros.php');
+    exit();
+} else {
+    header('Location: login.php?msg=Login ou senha incorretos.');
+    exit();
+}
+?>
